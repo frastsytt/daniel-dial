d7e7c565981b1cee6aab7d7a62d71c31432f2cfa
frastsytt <frastsytt@gmail.com>
2023-02-14 23:49:09 +0200

    chartjs fix

--- src/components/HistoryChart/HistoryChart.tsx
+++ src/components/HistoryChart/HistoryChart.tsx
@@ -2,22 +2,26 @@ import React, { useRef, useEffect, useState } from 'react';
 import type { ChartData, ChartArea } from 'chart.js';
 import {
 	Chart as ChartJS,
-	CategoryScale,
 	LinearScale,
+	CategoryScale,
+	BarElement,
 	PointElement,
 	LineElement,
-	Tooltip,
 	Legend,
+	Tooltip,
+	registerables as registerablesJS,
 } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
+ChartJS.register(...registerablesJS);
 
 ChartJS.register(
-	CategoryScale,
 	LinearScale,
+	CategoryScale,
+	BarElement,
 	PointElement,
 	LineElement,
-	Tooltip,
-	Legend
+	Legend,
+	Tooltip
 );
 
 interface HistoryChartProps {
